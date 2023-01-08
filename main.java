816da14ff4b8608bed366dd95f57abaec8456042
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-08 18:55:22 +0700

    Menambahkan Class Main untuk mengeksekusi program

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+
+
+    public static void main(String[] args) {
+        sewaHotel sewaHotel = new sewaHotel();
+        pegawai pegawai = new pegawai();
+        System.out.println("----------------------------------");
+        System.out.println("|| Selamat datang di Hotel Bayu ||");
+        System.out.println("----------------------------------");
+        System.out.println("\n");
+        pegawai.dataPegawai();
+        sewaHotel.sewaHotel();
+    
+
+
+    }
+}
