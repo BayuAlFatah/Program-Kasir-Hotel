13f99ae6ca373ed1711d66f2ad73aff84aae1166
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-10 14:13:39 +0700

    fix eror in main.java

--- main.java
+++ main.java
@@ -2,14 +2,14 @@ public class main {
 
 
     public static void main(String[] args) {
-        sewaHotel sewaHotel = new sewaHotel();
         pegawai pegawai = new pegawai();
         System.out.println("----------------------------------");
         System.out.println("|| Selamat datang di Hotel Bayu ||");
+        System.out.println("|| \t JL.Ahmad Yani 27 \t||");
         System.out.println("----------------------------------");
         System.out.println("\n");
          pegawai.dataPegawai();
-         sewaHotel.sewaHotel();
+         pegawai.sewaHotel();
     
 
 
